aba9a63c5b88ac994c61a863d3763f8753b46529
mohdsajeed <mohdsajeed871@gmail.com>
2023-12-27 22:21:06 +0530

    jwt code written

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,26 @@
 import asyncHandler from "../utils/asyncHandler.js";
 import apiError from "../utils/apiError.js";
 import { User } from "../models/user.model.js";
-import uploadOnCloudinary from "../utils/cloudinary.js"
+import uploadOnCloudinary from "../utils/cloudinary.js";
 import apiResponse from "../utils/apiResponse.js";
+
+const generateAccessAndRefreshTokens = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new apiError(
+      500,
+      "Something went wrong while generating refresh and access token"
+    );
+  }
+};
 
 const registerUser = asyncHandler(async (req, res) => {
   //get user details from frontend
@@ -16,7 +34,7 @@ const registerUser = asyncHandler(async (req, res) => {
   // return res
 
   //get user details from frontend
-  const { fullname, email, username, password } = req.body
+  const { fullname, email, username, password } = req.body;
   // console.log("email", email);
 
   //validation - not empty
@@ -29,13 +47,13 @@ const registerUser = asyncHandler(async (req, res) => {
   //check if user already exists:username, email
   const existedUser = await User.findOne({
     $or: [{ username }, { email }],
-  })
-  
+  });
+
   if (existedUser) {
-    throw new apiError(409, "User with email and username already exist")
+    throw new apiError(409, "User with email and username already exist");
   }
 
-// console.log(req.files)
+  // console.log(req.files)
 
   // check for images, check for avatar
 
@@ -43,10 +61,13 @@ const registerUser = asyncHandler(async (req, res) => {
   // const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
   let coverImageLocalPath;
-  if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
-    coverImageLocalPath = req.files.coverImage[0].path
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
   }
-
 
   if (!avatarLocalPath) {
     throw new apiError(400, "Avatar file is required");
@@ -56,16 +77,13 @@ const registerUser = asyncHandler(async (req, res) => {
   const avatar = await uploadOnCloudinary(avatarLocalPath);
   const coverImage = await uploadOnCloudinary(coverImageLocalPath);
 
-
   if (!avatar) {
     throw new apiError(400, "Avatar file is required");
   }
 
-  
   // if (!coverImage) {
   //   throw new apiError(400, "coverImage file is required");
   // }
-
 
   // create user object - create entry in db
   const user = await User.create({
@@ -75,12 +93,12 @@ const registerUser = asyncHandler(async (req, res) => {
     email,
     password,
     username: username.toLowerCase(),
-  })
+  });
 
   // remove password and refresh token field from response
   const createdUser = await User.findById(user._id).select(
     "-password -refreshToken"
-  )
+  );
 
   // check for user creation
   if (!createdUser) {
@@ -92,4 +110,91 @@ const registerUser = asyncHandler(async (req, res) => {
     .json(new apiResponse(200, createdUser, "User registered Successfully"));
 });
 
-export default registerUser;
+const loginUser = asyncHandler(async (req, res) => {
+  // req body -> data
+  // username or email
+  // find the user
+  // password check
+  // access and refresh token
+  // send cookies
+
+  // req body -> data
+  const { username, email, password } = req.body;
+
+  // username or email
+  if (!(username || email)) {
+    throw new apiError(400, "username or email is required");
+  }
+
+  // find the user
+  const user = User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (!user) {
+    throw new apiError(404, "user does not exist");
+  }
+
+  const isPasswordValid = await user.isPasswordCorrect(password);
+
+  if (!isPasswordValid) {
+    throw new apiError(401, "Invalid user credentials");
+  }
+
+  const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(
+    user._id
+  );
+
+  const loggedInUser = await User.findById(user._id).select(
+    "-password - refreshToken"
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .cookie("AccessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new apiResponse(
+        200,
+        {
+          user: loggedInUser,
+          accessToken,
+          refreshToken,
+        },
+
+        "User logged in Successfully"
+      )
+    );
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+  User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: {
+        refreshToken: undefined,
+      },
+    },
+    {
+      new: true,
+    }
+  )
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  }
+
+  return res
+  .status(200)
+  .clearCookie("accessToken",options)
+  .clearCookie("refreshToken",options)
+  .json(new apiResponse(200,{}, "User logged Out Successfully"))
+});
+
+export { registerUser, loginUser, logoutUser };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import registeUser from "../controllers/user.controller.js";
+import {logoutUser, registerUser} from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js";
 
 const router = Router();
@@ -15,8 +15,13 @@ router.route("/register").post(
       maxCount: 1,
     },
   ]),
-  registeUser
+  registerUser
 );
-// router.route("/Login").post(loginUser)
+
+router.route("/login").post(loginUser)
+
+
+//secured routes
+router.route("/logout").post(verifyJWT, logoutUser)
 
 export default router;
